b27a834479cb9ac104e2d4f6e0e40e213e2a87a5
Dylan Digby <dylan.m.c.digby@gmail.com>
2019-03-11 06:31:25 +0000

    Create article successfully

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "Creating Articles" do
+  
+  scenario "A user creates a new article" do
+    
+    visit "/"
+    
+    click_link "New Article"
+    
+    fill_in "Title", with: "Creating a blog"
+    fill_in "Body", with: "Test text"
+    
+    click_button "Create Article"
+    
+    expect(page).to have_content("Article has been created")
+    # expect(page).to eq(articles_path) # method not working because it returns "~<capybara session>"
+  end
+end
